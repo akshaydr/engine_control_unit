60d5b3428f3cbdbdc8fd13a30fa02bc124c7a80a
Akshay <akshay.dr.11@gmail.com>
2017-11-03 10:44:21 +0530

    Modified feedback.py added gui to description.

--- throttle_control/scripts/feedback.py
+++ throttle_control/scripts/feedback.py
@@ -7,41 +7,44 @@ from std_msgs.msg import Float32
 import time
 
 if RPI:
-    import RPi.GPIO as GPIO
-    GPIO.setmode(GP, IO.BOARD)
-    GPIO.setup(sensor, GPIO.IN)
+  import RPi.GPIO as GPIO
+  GPIO.setmode(GP, IO.BOARD)
+  GPIO.setup(sensor, GPIO.IN)
 
-startTime = 0
+current_time = rospy.Time()
+last_time = rospy.Time
 state = 0
 prevState = 0
 counter = 0.0
 
 sensor = 4
 
+
 def get_rpm():
-    if RPI:
-        sensor_val = GPIO.input(sensor)
+  if RPI:
+    sensor_val = GPIO.input(sensor)
 
-    if (sensor_val > 650):
-        state = 1;
-    else:
-        state = 0;
-    if (state != prevState):
-        if(state == 1):
-            counter++;
-        prevState = state;
+  if (sensor_val > 650):
+    state = 1
+  else:
+    state = 0
+  if (state != prevState):
+    if(state == 1):
+      counter += 1
+    prevState = state
 
-    while (time.time() - startTime) > 0.1:
-        print counter/6
-        counter = 0
-        startTime = time.time()
+  while (last_time - startTime) > 0.1:
+    print counter / 6
+    counter = 0
+    startTime = last_time
+
 
 if __name__ == '__main__':
-    global counter
-    rospy.init_node('feedback_val')
-    pub = rospy.Publisher('feedback_val', Float32, queue_size=10)
+  global counter
+  rospy.init_node('feedback_val')
+  pub = rospy.Publisher('feedback_val', Float32, queue_size=10)
 
-    while not rospy.is_shutdown():
-        get_rpm()
-        pub.publish(counter)
-        rospy.spin()
+  while not rospy.is_shutdown():
+    get_rpm()
+    pub.publish(counter)
+    rospy.spin()
