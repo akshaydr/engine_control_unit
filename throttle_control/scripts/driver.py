27f564b3182582d636a9190398f82808aca20746
akshaydr <akshay.dr.11@gmail.com>
2017-11-21 12:16:47 +0530

    Editted driver.py smooth motion without gaps

--- throttle_control/scripts/driver.py
+++ throttle_control/scripts/driver.py
@@ -9,7 +9,7 @@ import time
 if RPI:
   import RPi.GPIO as GPIO
 
-direction = ""
+direction = "stop"
 
 if RPI:
   GPIO.setmode(GPIO.BOARD)
@@ -27,29 +27,35 @@ if RPI:
 else:
   print("Simulated setup done!")
 
-
-def callback(msg):
-  direction = msg.data
+def motor_run():
+  global direction
   rospy.loginfo(direction)
   if RPI:
     if (direction == "cw"):
-      pwm1.start(50)
+      pwm1.start(100)
       GPIO.output(M1dir, GPIO.HIGH)
 
     elif (direction == "ccw"):
-      pwm1.start(50)
+      pwm1.start(100)
       GPIO.output(M1dir, GPIO.LOW)
 
-  elif (direction == "stop"):
+    elif (direction == "stop"):
       pwm1.stop()
       GPIO.output(M1dir, GPIO.LOW)
-
+ 
+def callback(msg):
+  global direction
+  direction = msg.data
 
 if __name__ == '__main__':
   rospy.init_node('driver')
   rospy.Subscriber('dirver_val', String, callback)
+  rate = rospy.Rate(60)
 
   while not rospy.is_shutdown():
-    rospy.spin()
-if RPI:
-  GPIO.cleanup()
+    motor_run()
+    rate.sleep()
+   
+
+  if RPI:
+    GPIO.cleanup()
